53f801f96577b532f31800cfa26fad5839c6f8e2
Wilhen Alberto Hui Mei <92064680+WilhenAlbertoHM@users.noreply.github.com>
2023-01-04 20:42:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-projects = [
+// Projects section.
+let projects = [
     {
-        image: "images/japan.jpg",
-        title: "Project 1"
+        image: "images/biblequiz.JPG",
+        title: "Bible Quiz Website (in-progress)"
     },
     {
-        image: "images/japan.jpg",
-        title: "Project 2"
+        image: "images/market.JPG",
+        title: "Market Website (in-progress)"
     },
     {
-        image: "images/japan.jpg",
-        title: "Project 3"
+        image: "images/numofislands.jpg",
+        title: "Number of Islands"
+    }, 
+    {
+        image: "images/tictactoe.JPG",
+        title: "Tic Tac Toe"
     },
     {
-        image: "images/japan.jpg",
-        title: "Project 4"
+        image: "images/physicsgrade.JPG",
+        title: "Grade Calculator"
     },
     {
-        image: "images/japan.jpg",
-        title: "Project 5"
-    },
-    {
-        image: "images/japan.jpg",
-        title: "Project 6"
-    },
+        image: "images/github.jpg",
+        title: "More details on GitHub...",
+        url: "https://wilhenalbertohm.github.io"
+    }
 ];
 
 // Iteratively create image grids for the "Projects" section.
@@ -38,6 +40,9 @@ function createProjectGrids(project) {
     const projectTitle = document.createElement("h3");
     projectTitle.classList.add("projects-title");
     projectTitle.textContent = project.title;
+    projectImage.addEventListener("click", function() {
+        window.location.assign(project.url);
+    });
     projectItem.appendChild(projectTitle);
 
     return projectItem;
@@ -52,8 +57,34 @@ projects.forEach(project => {
     projectGrid.appendChild(projectItem);
 });
 
-pictures = ["images/japan.jpg"];
+// Gallery section.
+let pictures = ["images/coolsunset.jpg", "images/jfklib.jpg", 
+    "images/montreal.jpg", "images/night.jpg", "images/nightumb2.jpg", 
+    "images/quincycenter.jpg", "images/sunset.jpg", "images/sunset2.jpg", "images/sunsetumb.jpg",
+    "images/sunsetwater.jpg", "images/toronto.jpg", "images/towertoronto.jpg", "images/trees.jpg", 
+    "images/uhall.jpg", "images/umbday.jpg"];
 
+
+// Iteratively create image grids for the "Gallery" section.
+function createGalleryGrids(image) {
+    const galleryItem = document.createElement("div");
+    galleryItem.classList.add("gallery-item");
+    
+    const galleryImage = document.createElement("img");
+    galleryImage.classList.add("gallery-img");
+    galleryImage.src = image;
+    galleryItem.appendChild(galleryImage);
+    return galleryItem;
+}
+
+// Get the project grid element.
+const galleryGrid = document.getElementById("gallery-grid");
+
+// Add project items to the grid.
+for (let picture = 0; picture < pictures.length; picture++) {
+    const galleryItem = createGalleryGrids(pictures[picture]);
+    galleryGrid.appendChild(galleryItem);
+}
 
 // When one of the nav bar links is clicked, page will scroll down to
 // the desired section.
